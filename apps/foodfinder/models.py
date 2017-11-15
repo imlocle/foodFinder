36d3f6179da0b1f9c3099c8e20340aa3398f29a3
The Flash <theflash@Legacys-iMac.local>
2017-11-14 19:26:29 -0800

    Models views settings

--- apps/foodfinder/models.py
+++ apps/foodfinder/models.py
@@ -1,6 +1,73 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
-
+import re
+import bcrypt
+from datetime import datetime
+EMAILCHECK = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 # Create your models here.
+
+class UserManager(models.Manager):
+    def checkreg(self, name, email, password, confirm_password):
+        errorlist = []
+        count = 0
+        now = datetime.now()
+        if len(name)<2:
+            errorlist.append('First name too short!')
+            count +=1
+
+        if len(email)<1:
+            errorlist.append('Email is required!')
+            count+=1
+        elif not EMAILCHECK.match(email):
+            errorlist.append('Please enter a valid email!')
+            count+=1
+
+        if not password==confirm_password:
+            errorlist.append('Passwords are not the same!')
+            count+=1
+        elif len(password)<8:
+            errorlist.append('Please enter a password longer than 8 characters!')
+            count+=1
+
+        if count==0:
+            return True
+        return errorlist
+
+    def checklog(self, email, password):
+        errorlist = []
+        count = 0
+        if User.objects.filter(email=email).count()<1:
+            count+=1
+            errorlist.append('Email incorrect')
+            return errorlist
+        user = User.objects.get(email=email)
+        password=password.encode()
+        print bcrypt.hashpw(password, bcrypt.gensalt())
+        print user.password
+        if bcrypt.hashpw(password, user.password.encode()) != user.password:
+            count+=1
+            errorlist.append('Password incorrect')
+
+        if count == 0:
+            return [True, user]
+        return errorlist
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+    life_style = models.CharField(max_length = 200, null = True, blank = True)
+    i_am = models.CharField(max_length = 300, null = True, blank = True)
+    rating = models.CharField(max_length = 100, null = True, blank = True)
+    popularity = models.CharField(max_length = 200, null = True, blank= True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    userManager = UserManager()
+    objects = models.Manager()
+
+class UserPlace(models.Model):
+    user = models.ForeignKey(User, related_name = 'place_user')
+    place_url = models.CharField(max_length = 200, default='')
+    name = models.CharField(max_length = 200, default='')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
--- apps/foodfinder/views.py
+++ apps/foodfinder/views.py
@@ -1,6 +1,137 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User, UserPlace
+import bcrypt
+from django.contrib import messages
+from datetime import datetime
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+from django.http import JsonResponse
+import json
 
 # Create your views here.
+auth = Oauth1Authenticator(
+    consumer_key='QALWzkvikT2OdQXpp5u0BQ',
+    consumer_secret='N96o0rgo_cAKkI2xVUX9DOTgs9A',
+    token='M-0HG4VGCuLTTU-Yrz7IxZJDggdZ8wNX',
+    token_secret='5XrSq-6WFQiYclv3K1mCBlVcHRw'
+)
+
+client = Client(auth)
+
+
+
+def index(request):
+    return render(request, 'wild/index.html')
+
+
+def login(request):
+    check = User.userManager.checklog(request.POST['loginemail'], request.POST['loginpass'])
+
+    if check[0] == True:
+        request.session['id']=check[1].id
+        return redirect('/main')
+    else:
+        for error in check:
+            messages.error(request, error)
+        return redirect ('/loginreg')
+
+def register(request):
+    check = User.userManager.checkreg(request.POST['name'], request.POST['email'], request.POST['pass'], request.POST['conf_pass'])
+
+    if check == True:
+        passinput = request.POST['pass'].encode()
+        hashed = bcrypt.hashpw(passinput, bcrypt.gensalt())
+        the_user = User.objects.create(name=request.POST['name'], email=request.POST['email'], password=hashed)
+        request.session['id'] = the_user.id
+        return redirect('/preferences/'+ str(the_user.id))
+    else:
+        for error in check:
+            messages.error(request, error)
+        return redirect('/loginreg')
+
+def loginreg(request):
+    return render(request, 'wild/loginreg.html')
+
+
+def main(request):
+    the_user = User.objects.get(id = request.session['id'])
+    if the_user:
+        if 'city' in request.session:
+            del request.session['city']
+            del request.session['life_style']
+            del request.session['rating']
+            del request.session['i_am']
+            del request.session['popularity']
+        context = {'user': the_user}
+        return render(request, 'wild/main.html', context)
+    else:
+        return redirect('/loginreg')
+
+def results(request,num):
+    if not 'city' in request.session:
+        request.session['city'] = request.POST['city']
+        request.session['life_style']=request.POST['life_style']
+        request.session['rating']=request.POST['rating']
+        request.session['i_am']=request.POST['i_am']
+        request.session['popularity']=request.POST['popularity']
+    if request.session['popularity']=='high':
+        popularity = 100
+    elif request.session['popularity']=='medium':
+        popularity = 50
+    else:
+        popularity = 0
+
+    if request.session['i_am']=='classy':
+        alchohol = ',beer_and_wine'
+    elif request.session['i_am']=='mess':
+        alchohol = ',bars'
+    else:
+        alchohol = ''
+    print
+    params = {
+        'term': 'food',
+        'category_filter':request.session['life_style']+alchohol,
+        'rating':request.session['rating'],
+        'review_count':popularity,
+    }
+    response = client.search(request.session['city'], **params)
+    print '**************'
+    for i in range(0, 20):
+        print response.businesses[i].name
+        print '%%%%%%%%%%%%%%%%%%'
+        print response.businesses[i].url
+    print len(response.businesses)
+    if int(num)==20:
+        num=0
+    context = {
+        'response':response.businesses[int(num)],
+        'num': int(num)+1,
+    }
+    # result = JsonResponse(response, safe=False)
+    return render(request,'wild/app.html', context)
+
+def accepted(request):
+    UserPlace.objects.create(user=User.objects.get(id=request.session['id']), place_url=request.POST['url'], name=request.POST['name'])
+    return redirect('/list')
+
+def logout(request):
+    del request.session['id']
+    return redirect('/')
+
+def list(request):
+    joined=UserPlace.objects.filter(user=User.objects.get(id=request.session['id']))
+    context = {
+        'joined':joined,
+    }
+    return render(request, 'wild/list.html', context)
+
+def preferences(request, id):
+    return render(request, 'wild/preferences.html')
+
+def record(request):
+    the_user = User.objects.filter(id = request.session['id']).update(life_style = request.POST['life_style'], i_am = request.POST['alcohol'], rating = request.POST['rating'], popularity = request.POST['popularity'])
+    return redirect('/main')
+
+def wildrecord(request):
+    the_user = User.objects.filter(id = request.session['id']).update(life_style = request.POST['life_style'], i_am = request.POST['alcohol'], rating = '1', popularity = '0')
+    return redirect('/main')
